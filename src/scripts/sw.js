bcdd68d408ecf997150d6b4b52b3ee4061bc31d6
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-06-11 18:29:54 +0700

    add cache api

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,19 +1,16 @@
-self.addEventListener('install', () => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
-  // TODO: Caching App Shell Resource
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
-self.addEventListener('activate', () => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
