111f7acc776ed2d8ca23eed40a85b8a90158818f
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-12-07 16:14:09 +0700

    update API endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,7 +1,7 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-  HOME: `${CONFIG.BASE_URL}list/`,
+  HOME: `${CONFIG.BASE_URL}list`,
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
 };
 
